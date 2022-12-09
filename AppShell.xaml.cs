e3399d0f3fffab35850be9c891ba39a5aab027c6
Ngali-coder <115790396+Ngali-coder@users.noreply.github.com>
2022-12-09 23:30:57 +0100

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,12 @@
+
+namespace Animations;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+
+        Routing.RegisterRoute(nameof(EasingEditorPage), typeof(EasingEditorPage));
+    }
+}
---
+++ ngali.xaml.cs
@@ -0,0 +1,12 @@
+
+namespace Animations;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
